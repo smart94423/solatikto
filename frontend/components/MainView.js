80a6a236e70c0880c58167666e1ee08fd9c86e74
smart94423 <smart94423@gmail.com>
2022-11-16 07:25:04 +0300

    build main structure

--- frontend/components/MainView.js
+++ frontend/components/MainView.js
@@ -1,4 +1,7 @@
 import React from "react";
+import UploadModal from "./UploadModal";
+import Video from "./Video";
+import { BottomBar } from "./BottomBar";
 import styles from "../styles/MainView.module.css";
 import Signup from "./Signup";
 import { useEffect, useState } from "react";
@@ -10,10 +13,10 @@ import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import useAccount from "../hooks/useAccount";
 import useTiktok from "../hooks/useTiktok";
 
-const anchor = require("@project-serum/anchor")
-const utf8 = anchor.utils.bytes.utf8
-const { BN, web3 } = anchor
-const { SystemProgram } = web3
+const anchor = require("@project-serum/anchor");
+const utf8 = anchor.utils.bytes.utf8;
+const { BN, web3 } = anchor;
+const { SystemProgram } = web3;
 
 const defaultAccounts = {
   tokenProgram: TOKEN_PROGRAM_ID,
@@ -31,61 +34,64 @@ const MainView = () => {
 
   const [tiktoks, setTiktoks] = useState();
   const [newVideoShow, setNewVideoShow] = useState(false);
-  const [description, setDescription] = useState('');
-  const [videoUrl, setVideoUrl] = useState('h');
-  const [userDetail, SetUserDetail] = useState();
+  const [description, setDescription] = useState("");
+  const [videoUrl, setVideoUrl] = useState("hello");
+  const [userDetail, setUserDetail] = useState();
 
-  const { signup } = useAccount()
-  const { getTiktoks, likeVideo, createComment, newVideo,getComments } = useTiktok(
-    setTiktoks,
-    userDetail,
-    videoUrl,
-    description,
-    setDescription,
-    setVideoUrl,
-    setNewVideoShow,
-
-
-  )
+  const { signup } = useAccount();
+  const { getTiktoks, likeVideo, createComment, newVideo, getComments } =
+    useTiktok(
+      setTiktoks,
+      userDetail,
+      videoUrl,
+      description,
+      setDescription,
+      setVideoUrl,
+      setNewVideoShow
+    );
 
   useEffect(() => {
     if (wallet.connected) {
-      checkAccount()
-      getTiktoks()
+      checkAccount();
+      getTiktoks();
     }
-  }, [wallet.connected])
+  }, [wallet.connected]);
 
   const checkAccount = async () => {
     let [user_pda] = await anchor.web3.PublicKey.findProgramAddress(
       [utf8.encode("user"), wallet.publicKey.toBuffer()],
-      program.programId,
-    )
+      program.programId
+    );
     try {
       const userInfo = await program.account.userAccount.fetch(user_pda);
       console.log(userInfo);
       setUserDetail(userInfo);
-      setAccount(true)
-
+      setAccount(true);
     } catch (e) {
-      setAccount(false)
+      setAccount(false);
     }
-
-  }
-
+  };
 
   return (
     <>
       {isAccount ? (
         <div>
-          Tiktoks will go here
+          {newVideoShow && <UploadModal />}
+          <div className="styles.appVideos">
+            {tiktoks.length === 0 ? (
+              <h1> No videos </h1>
+            ) : (
+              titktoks.map((tiktok, id) => <Video />)
+            )}
+          </div>
 
+          <BottomBar />
         </div>
-      )
-        : (
-          <Signup signup={signup} wallet={wallet.publicKey.toBase58()} /> //pass props to children
-        )}
+      ) : (
+        <Signup signup={signup} wallet={wallet.publicKey.toBase58()} /> //pass props to children
+      )}
     </>
-  )
+  );
 };
 
 export default MainView;
